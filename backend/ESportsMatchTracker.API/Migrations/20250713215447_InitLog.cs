96107a4b326193196b6f4342a3f8b07354c5c84c
Frost <frost.chen.c.k@gmail.com>
2025-07-14 06:07:42 +0800

    EF migrations

---
+++ backend/ESportsMatchTracker.API/Migrations/20250713215447_InitLog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESportsMatchTracker.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApiLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Path = table.Column<string>(type: "TEXT", nullable: false),
+                    Method = table.Column<string>(type: "TEXT", nullable: false),
+                    StatusCode = table.Column<int>(type: "INTEGER", nullable: false),
+                    RequestBody = table.Column<string>(type: "TEXT", nullable: true),
+                    ResponseBody = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiLogs");
+        }
+    }
+}
